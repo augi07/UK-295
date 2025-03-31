8047abd1d5824a59c9b33be604c3e6e448eba315
augi07 <102184778+augi07@users.noreply.github.com>
2025-03-31 16:44:01 +0200

    first commit

---
+++ src/main/java/ch/csbe/productstore/CategoryRepository.java
@@ -0,0 +1,9 @@
+package ch.csbe.productstore;
+
+import ch.csbe.productstore.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+}
---
+++ src/main/java/ch/csbe/productstore/Product.java
@@ -0,0 +1,56 @@
+package ch.csbe.productstore;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private double price;
+
+    @ManyToOne
+    @JoinColumn(name = "category_id")
+    private Category category;
+
+    // Konstruktoren
+    public Product() {}
+
+    public Product(String name, double price, Category category) {
+        this.name = name;
+        this.price = price;
+        this.category = category;
+    }
+
+    // Getter & Setter
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+}
