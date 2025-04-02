077f4251d35139153c519e0f25ac5f6a36b501cf
augi07 <102184778+augi07@users.noreply.github.com>
2025-04-02 09:37:40 +0200

    sql Update

--- src/main/resources/db/migration/V1__init.sql
+++ src/main/resources/db/migration/V1__init.sql
@@ -1,31 +1,31 @@
 -- Erstellt die User Tabelle
 CREATE TABLE IF NOT EXISTS user (
-                                    id BIGINT AUTO_INCREMENT PRIMARY KEY,
-                                    email VARCHAR(255) NOT NULL UNIQUE,
-    password VARCHAR(255) NOT NULL
+        id BIGINT AUTO_INCREMENT PRIMARY KEY,
+        email VARCHAR(255) NOT NULL UNIQUE,
+        password VARCHAR(255) NOT NULL
     );
 
 -- Erstellt die User-Rollen Tabelle
 CREATE TABLE IF NOT EXISTS user_roles (
-                                          user_id BIGINT NOT NULL,
-                                          role VARCHAR(255) NOT NULL,
-    FOREIGN KEY (user_id) REFERENCES user(id)
+        user_id BIGINT NOT NULL,
+        role VARCHAR(255) NOT NULL,
+        FOREIGN KEY (user_id) REFERENCES user(id)
     );
 
 -- Erstellt die Kategorie Tabelle
 CREATE TABLE IF NOT EXISTS category (
-                                        id BIGINT AUTO_INCREMENT PRIMARY KEY,
-                                        name VARCHAR(255) NOT NULL
+        id BIGINT AUTO_INCREMENT PRIMARY KEY,
+        name VARCHAR(255) NOT NULL
     );
 
 -- Erstellt die Produkt Tabelle
 CREATE TABLE IF NOT EXISTS product (
-                                       id BIGINT AUTO_INCREMENT PRIMARY KEY,
-                                       name VARCHAR(255) NOT NULL,
-    description TEXT,
-    price DOUBLE,
-    category_id BIGINT,
-    FOREIGN KEY (category_id) REFERENCES category(id)
+        id BIGINT AUTO_INCREMENT PRIMARY KEY,
+        name VARCHAR(255) NOT NULL,
+        description TEXT,
+        price DOUBLE,
+        category_id BIGINT,
+        FOREIGN KEY (category_id) REFERENCES category(id)
     );
 
 -- Admin Benutzer einfügen (Passwort = "admin123" bcrypt-hash)
